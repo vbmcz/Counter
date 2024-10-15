4c4d8e911df3996fa6acc1ca9c75baf046289d7a
vbmcz <151908083+vbmcz@users.noreply.github.com>
2024-10-15 17:25:41 +0200

    (comp) + pushing it slowly forward

--- Models/AllCounters.cs
+++ Models/AllCounters.cs
@@ -3,11 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Counter.Views;
 
 namespace Counter.Models
 {
-    class AllCounters
+    class AllCounters : Page
     {
+
+        public AllCounters() {
+            CreateButton();
+        }
+
+
+        private void CreateButton()
+        {
+            int counter = 0;
+
+            var layout = new VerticalStackLayout();
+
+            Label label = new Label
+            {
+                Text = $"Counter: {counter}",
+            };
+
+            Button button = new Button
+            {
+                Text = "OK",
+                Command = new Command(
+                    execute: () =>
+                    {
+                        counter += 1;
+                        label.Text = $"Counter: {counter}";
+                    }
+                )
+            };
+            layout.Children.Add( label );
+            layout.Children.Add( button );
+
+            AllCountersPage allCountersPage = new()
+            {
+                Content = layout
+            };
+        }
         
     }
 }
