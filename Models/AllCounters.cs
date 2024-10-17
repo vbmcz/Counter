a235c6aaed43511336666a1d7c40b5f2f96c0969
vbmcz <151908083+vbmcz@users.noreply.github.com>
2024-10-17 10:30:02 +0200

    idk anymore

--- Models/AllCounters.cs
+++ Models/AllCounters.cs
@@ -10,45 +10,5 @@ namespace Counter.Models
     class AllCounters : ContentPage
     {
 
-        public AllCounters()
-        {
-            this.CreateButton();
-        }
-
-
-        private void CreateButton()
-        {
-			int counter = 0;
-
-			Label label = new Label
-			{
-				Text = $"Counter: {counter}",
-			};
-
-			Button button = new Button
-			{
-				Text = "OK",
-				Command = new Command(
-					execute: () =>
-					{
-						counter += 1;
-						label.Text = $"Counter: {counter}";
-					}
-				)
-			};
-
-			Label lblName = new Label
-			{
-				Text = "AA",
-			};
-
-			BatchBegin();
-
-            Content = lblName;
-
-			BatchCommit();
-		
-        }
-        
     }
 }
